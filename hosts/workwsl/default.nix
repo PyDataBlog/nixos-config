cd0e5d2c577b417629bce8bee6f1017fbd38943f
PyDataBlog <pimpfada@gmail.com>
2026-04-13 18:23:33 +0200

    wip: attempt to use certificate file

--- hosts/workwsl/default.nix
+++ hosts/workwsl/default.nix
@@ -13,11 +13,14 @@
 
   wsl.defaultUser = repoLib.primaryUser.username;
 
+  users.mutableUsers = false;
   security.sudo.wheelNeedsPassword = lib.mkForce true;
 
   programs.nix-ld.enable = true;
 
   sops.age.keyFile = lib.mkDefault "${repoLib.primaryUser.homeDirectory}/.config/sops/age/keys.txt";
+
+  repo.workNetwork.certificateFile = /home/bebr/zscaler.pem;
 
   repo.user = lib.mkDefault (
     repoLib.primaryUser
